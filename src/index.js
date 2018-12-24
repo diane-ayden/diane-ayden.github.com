61ebea8f092ac00eb23811fdb29d49f6c509c134
diane-ayden <46037301+diane-ayden@users.noreply.github.com>
2018-12-24 13:38:40 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+render(<App />, document.getElementById('app'));
