c183a78df6bca2d8f62c02978e0e7dc02587f9dd
diane-ayden <46037301+diane-ayden@users.noreply.github.com>
2018-12-24 13:38:16 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div></div>
+            </BrowserRouter>
+        );
+    }
+
+}
+
+export default App;
